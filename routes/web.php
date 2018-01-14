c07a299c957fe1a34c28c82f44fa4a582c4f2b21
Amrani Houssain <amranidev@gmail.com>
2018-01-14 00:28:00 +0000

    stupid routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'chat'], function () {
+
+    Route::get('/{threadID?}', "MainController@index");
+
+    Route::get('groups', function () {
+        return "Groups";
+    });
+
+    Route::post('store', 'MainController@store');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
